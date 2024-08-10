3f240ef6f1a3c996bfccbb9c14a0f0fa22dfe434
shashank-alt <146576463+shashank-alt@users.noreply.github.com>
2024-08-10 11:26:25 +0530

    1st assignment is added

---
+++ outputs_and_variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"1.Physics\nWallah";
+    cout<<endl;
+    cout<<"2. "<<5+5;
+    cout<<endl;
+    cout<<"3."<<"   1. 4 bytes\n     2. 1 byte\n     3. 4 bytes";
+    cout<<endl;
+    int a= 4,b=5;
+    a++,b--;
+    cout<<"4. value of a: "<<++a<<" and b: "<<b--;
+    cout<<endl;
+    int radius = 10;
+    int circumference = 2*3.1415*radius;
+    cout<<"5. Circumference of the circle of radius: "<<radius<<endl;
+
+}
